ab9a12480ba5230484c3ed6e3ff49c37899573b0
felipenic@bol.com.br <felipenic@bol.com.br>
2012-04-22 21:54:49 -0300

    Teste comentario 1 domingo 22-04

--- segundafelipe/src/segunda/pacote/namespace/SegundafelipeActivity.java
+++ segundafelipe/src/segunda/pacote/namespace/SegundafelipeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class SegundafelipeActivity extends Activity {
     /** Called when the activity is first created. */
+	/** comentário 1*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
